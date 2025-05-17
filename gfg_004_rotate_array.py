d50af7b3a2e73fbcb3a2cc8e44177e9534a61c9e
tonybnya <nya.tony2010@gmail.com>
2025-05-17 11:14:08 +0100

    feat: add O(1) SC & O(n) TC solution using Reverse Inward Traversal with Two Pointers for problem 004 Rotate Array

--- gfg_004_rotate_array.py
+++ gfg_004_rotate_array.py
@@ -69,3 +69,39 @@ def rotate_array_2(arr: list[int], d: int) -> list[int]:
         i += 1
 
     return temp
+
+
+def rotate_array_3(arr: list[int], d: int) -> list[int]:
+    """
+    Solution 3: Reverse in place using Inward Traversal Two Pointers
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+    """
+
+    def rev(arr: list[int], left: int, right: int) -> None:
+        """
+        Helper function to reverse an array in place.
+        Time Complexity: O(n)
+        Space Complexity: O(1)
+        """
+        while left < right:
+            arr[left], arr[right] = arr[right], arr[left]
+            left += 1
+            right -= 1
+
+    n: int = len(arr)
+    if d in (0, n):
+        return arr
+    if d > n:
+        d %= n
+
+    # step 1: reverse the first d elements
+    rev(arr, 0, d - 1)
+
+    # step 1: reverse elements from d index to the end
+    rev(arr, d, n - 1)
+
+    # step 1: reverse the entire array
+    rev(arr, 0, n - 1)
+
+    return arr
